f00f111b69b5d0afd2a92b5a6c544ec7059a266a
tdewangan <tribhuwan.dewangan@gmail.com>
2017-12-25 19:44:06 +0530

    redux observables working example

--- App.js
+++ App.js
@@ -1,31 +1,14 @@
 import React from 'react';
-import { StyleSheet, View, Button, Text } from 'react-native';
+import { Provider } from 'react-redux';
 import store from './src/store';
-import { fetchUser } from './src/users';
+import UserScreen from './src/UserScreen';
 
 export default class App extends React.Component {
   render() {
-    const { users } = store.getState();
-    const user = users['tdewangan'] || '';
-    console.log('app.js', users);
     return (
-      <View style={styles.container}>
-        <Button
-          raised
-          title="getUser"
-          onPress={() => store.dispatch(fetchUser('tdewangan'))}
-        />
-        <Text>hi {user.name}</Text>
-      </View >
+      <Provider store={store}>
+        <UserScreen />
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/UserScreen.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { View, Text, Button } from 'react-native';
+import { connect } from 'react-redux';
+import { fetchUser } from './users';
+
+class UserScreen extends Component {
+    render() {
+        const users = this.props.users;
+        const user = users['tdewangan'] || '';
+        return (
+            <View>
+                <Text>Hello</Text>
+                <Text>{user.name}</Text>
+                <Button
+                    title="getUserInfo"
+                    onPress={() => this.props.fetchUser('tdewangan')}
+                />
+            </View>
+        );
+    }
+}
+
+const mapStateToProps = ({ users }) => ({ users });
+export default connect(mapStateToProps, { fetchUser })(UserScreen);
